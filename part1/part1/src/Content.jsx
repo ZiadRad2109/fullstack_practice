2a22a8157d6840b3f90da131eff72efbedc13b8d
Ziad Radwan <ziad.radwan@cyshield.com>
2026-08-14 16:15:21 +0300

    add parts upt to 1.5

--- part1/part1/src/Content.jsx
+++ part1/part1/src/Content.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Part from './Part'
 
-const Content = (props) => {
+const Content = ({parts}) => {
+    const partsarray = parts.map(value=>(<Part key={value.name} name = {value.name} exercise={value.exercises}/>))
     return (
-        <div>    
+        <div >    
+            {partsarray}
 
-            <Part name={props.part1} exercise={props.exercises1} />
+            {/* <Part name={props.part1.name} exercise={props.part1.exercises} />
 
-            <Part name={props.part2} exercise={props.exercises2} />
+            <Part name={props.part2.name} exercise={props.part2.exercises} />
             
-            <Part name={props.part3} exercise={props.exercises3} />
+            <Part name={props.part3.name} exercise={props.part3.exercises} /> */}
             </div>
     )
 }
--- part1/part1/src/Total.jsx
+++ part1/part1/src/Total.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Total = (props) => {
-  return (
-          <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+const Total = ({parts}) => {
+ // parts is an array of of objects containing 'name' -> string and 'exercises' -> integers
+  const total = parts.reduce((sum,part)=>sum+part.exercises,0)
+ return (
+    //return the sum of elements within the parts array.exercises
+    <p>Number of exercises = {total}</p>
 
   )
 }
